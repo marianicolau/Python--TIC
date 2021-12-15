64da0b3472172ce0b053280231141fb472f991a2
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-15 10:36:23 +0100

    Update and rename ex03.4.py to ex03.5.py

--- ex03.5.py
+++ ex03.5.py
@@ -1,3 +1,12 @@
 """
 Este código que comprueba si x es un valor positivo. Hay dos cosas mal en él. Una impide que se ejecute, la otra es más sutil. Asegúrate de que la sentencia if funciona independientemente del valor que tome x. Identifícalas.
 """
+#exercicis capítol 03
+#maria nicolau jaume
+
+x = float(input("Digues un número"))
+if x >= 0:
+    print("x es positivo.")
+else:
+    print("x no es positivo.")
+    

2a5a0cff955f6f2d5f408a96c77f24c08d0b59d1
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-15 10:44:27 +0100

    Create ex03.7.py

---
+++ ex03.7.py
@@ -0,0 +1,18 @@
+"""
+Hay cuatro errores en este código. Identifícalos. 
+"""
+#exercicis del capítol 03
+#maria nicolau jaume
+"""
+respuesta = input("¿Cuál es el nombre del compañero de Sherlock Holmes?")
+if r = "Watson":
+    print("Correcto!")
+    else
+    print("Incorrecto! Es Watson.")
+"""
+
+respuesta = input("¿Quin es el nom del company de Sherlock Holmes?")
+if respuesta == "Watson":
+    print("Correcte!")
+else:
+    print("Incorrecte! Es Watson.")

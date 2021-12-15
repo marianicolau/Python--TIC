1a0192c3de267944ced14099bd5ec0b57386c5bf
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-15 10:35:43 +0100

    Add files via upload

---
+++ ex03.4.py
@@ -0,0 +1,3 @@
+"""
+Este código que comprueba si x es un valor positivo. Hay dos cosas mal en él. Una impide que se ejecute, la otra es más sutil. Asegúrate de que la sentencia if funciona independientemente del valor que tome x. Identifícalas.
+"""

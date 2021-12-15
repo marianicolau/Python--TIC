8c7758624a79a544dd8e0c24db10df827a9c8bcf
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-15 10:40:59 +0100

    Create ex03.6.py

---
+++ ex03.6.py
@@ -0,0 +1,14 @@
+"""
+¿Qué tres cosas están mal en este código?
+"""
+#exercicis del capítol 03
+#maria nicolau jaume
+"""
+x = input("Introduce un número:")
+if x = 3
+    print ("Has introducido el 3")
+"""
+
+x = int(input("Digues un número:"))
+if x == 3:
+    print ("Has dit el número 3")

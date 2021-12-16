59e024b18fd7c90b7f0593dfcfa1355c1cf7189b
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-16 11:48:29 +0100

    Add files via upload

---
+++ ex0.3.8.py
@@ -0,0 +1,11 @@
+"""
+Este programa no funciona bien. ¿Por qué?
+"""
+#exercici 8 del capítol 03
+#maria nicolau jaume
+
+x = input("Com estàs avui?")
+if x == "Feliç" or x == "Satisfet" or x == "Be":
+    print ("M'alegra escoltar això!")
+else:
+    print ("Milloret!")

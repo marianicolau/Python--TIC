f8006f19bcd0c0b15925535f2956e8c713cc7b2e
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-17 09:32:48 +0100

    Add files via upload

---
+++ ex04.1.py
@@ -0,0 +1,9 @@
+"""
+Escribe un programa en Python que, usando un bucle for, imprima 10 veces tu nombre y la palabra ``Hecho'' al final.
+"""
+#exercici 1 del capítol 04
+#maria nicolau jaume
+
+for i in range(10):
+    print ("Maria Nicolau")
+print ("fet")

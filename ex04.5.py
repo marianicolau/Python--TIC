c6a3e3b66b4b1034e04b2a8df255e60f654635ca
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-20 10:08:51 +0100

    Add files via upload

---
+++ ex04.5.py
@@ -0,0 +1,25 @@
+"""
+Este programa contiene tres errores. Identifica cada uno de ellos. 
+
+print("Este programa recibe tres números y devuelve la suma.")
+total = 0
+ 
+for i in range(3):
+    x = input("Introduce un número: ")
+    total = total + i
+print("El total es:", x)
+"""
+#exercici 5 del capítol 04
+#maria nicolau jaume
+
+print("Este programa recibe tres números y devuelve la suma.")
+total = 0
+ 
+for i in range(1):
+    x = int(input("Introduce un número: "))
+for i in range(1):
+    y = int(input("Introduce un número: "))
+for i in range(1):
+    z = int(input("Introduce un número: "))
+num = total + x + y + z
+print("El total es:", num)

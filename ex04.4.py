fdfff398ae7aed32603924c8b45956a49df74666
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-20 10:08:17 +0100

    Update ex04.4.py

--- ex04.4.py
+++ ex04.4.py
@@ -1,7 +1,7 @@
 """
 Escribe un programa en Python que, usando un bucle while, cuente hacia atrás desde el 10 hasta el 0, éste último incluido. Y que luego imprima la palabra ``Despegamos!''. Recuerda, usa un bucle WHILE, no vayas a hacerlo con un bucle FOR.
 """
-#exercici 3 del capítol 04
+#exercici 4 del capítol 04
 #maria nicolau jaume
 
 i = 10

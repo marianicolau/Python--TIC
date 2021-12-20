044604dee1ad5a53fed724c18f0db11158148f88
marianicolau <93319827+marianicolau@users.noreply.github.com>
2021-12-20 10:22:06 +0100

    Add files via upload

---
+++ ex04.6.py
@@ -0,0 +1,9 @@
+"""
+Escribe un programa que imprima un número entero al azar entre 1 y 10 (ambos incluidos).
+"""
+#exercici 6 del capítol 04
+#maria nicolau jaume
+
+x = input("Número aleatori entre 1 i 10")
+import random
+print(random.randrange(1,10))

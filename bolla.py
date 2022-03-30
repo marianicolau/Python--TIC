6793e315b087e61a95af969d28786d6649e1bcb8
marianicolau <93319827+marianicolau@users.noreply.github.com>
2022-03-30 10:46:08 +0200

    Add files via upload

---
+++ bolla.py
@@ -0,0 +1,85 @@
+import arcade
+import math
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+class Ball:
+    """ This class manages a ball bouncing on the screen. """
+
+    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
+        """ Constructor. """
+
+        # Take the parameters of the init function above, and create instance variables out of them.
+        self.position_x = position_x
+        self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
+        self.radius = radius
+        self.color = color
+
+    def draw(self):
+        """ Draw the balls with the instance variables we have. """
+        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
+
+    def update(self):
+        """ Code to control the ball's movement. """
+
+        # Move the ball
+        self.position_y += self.change_y
+        self.position_x += self.change_x
+
+        # See if the ball hit the edge of the screen. If so, change direction
+        if self.position_x < self.radius:
+            self.change_x *= -1
+
+        if self.position_x > SCREEN_WIDTH - self.radius:
+            self.change_x *= -1
+
+        if self.position_y < self.radius:
+            self.change_y *= -1
+
+        if self.position_y > SCREEN_HEIGHT - self.radius:
+            self.change_y *= -1
+
+def colisio(a,b):
+    distancia = math.sqrt(math.pow((a.position_x - b.position_x),2) + math.pow((a.position_y - b.position_y),2))
+    print(distancia)
+    if distancia <= (a.radius):
+        return True
+    else:
+        return False
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+
+        # Call the parent class's init function
+        super().__init__(width, height, title)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.ASH_GREY)
+
+                # Attributes to store where our ball is
+        self.ball = Ball(50, 50, 3, 3, 20, arcade.color.AUBURN)
+        self.ball2 = Ball(150, 50, 3, 7, 20, arcade.color.AUBURN)
+
+    def on_draw(self):
+        """ Called whenever we need to draw the window. """
+        arcade.start_render()
+        self.ball.draw()
+        self.ball2.draw()
+
+    def update(self, delta_time):
+        """ Called to update our objects. Happens approximately 60 times per second."""
+        colisio(self.ball,self.ball2)
+        self.ball2.update()
+        self.ball.update()
+
+def main():
+    window = MyGame(640, 480, "Drawing Example")
+
+    arcade.run()
+
+
+main()

f85bd2bd5af389b43e5bea74404a13c0239f74fc
marianicolau <93319827+marianicolau@users.noreply.github.com>
2022-05-05 11:24:27 +0200

    Add files via upload

--- novaBolla.py
+++ novaBolla.py
@@ -4,12 +4,6 @@ import random
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-<<<<<<< HEAD
-#def sol (px,py):
-#    arcade.draw_circle_filled(20+px, 360+py, 30, arcade.csscolor.YELLOW)
-
-=======
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
 class Ball:
     """ This class manages a ball bouncing on the screen. """
 
@@ -52,45 +46,6 @@ class Ball:
         if self.position_y > SCREEN_HEIGHT - self.radius:
             self.change_y *= -1
 
-<<<<<<< HEAD
-class Ninot(Ball):
-    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
-        """ Constructor. """
-
-        # Take the parameters of the init function above, and create instance variables out of them.
-        self.position_x = position_x
-        self.position_y = position_y
-        self.change_x = change_x
-        self.change_y = change_y
-        self.radius = radius
-        self.color = color
-    
-    def draw(self):
-        """ Draw the balls with the instance variables we have. """
-        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
-    def update(self):
-        pass
-
-class Tronc(Ball):
-    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
-        """ Constructor. """
-
-        # Take the parameters of the init function above, and create instance variables out of them.
-        self.position_x = position_x
-        self.position_y = position_y
-        self.change_x = change_x
-        self.change_y = change_y
-        self.radius = radius
-        self.color = color
-    
-    def draw(self):
-        """ Draw the balls with the instance variables we have. """
-        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
-    def update(self):
-        pass
-
-=======
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
 def colisio(a,b):
     distancia = math.sqrt(math.pow((a.position_x - b.position_x),2) + math.pow((a.position_y - b.position_y),2))
     #print(distancia)
@@ -102,11 +57,6 @@ def colisio(a,b):
 class Floc(Ball):
     def __init__(self, position_x, position_y, change_x, change_y, radius, color):
         super().__init__(position_x, position_y, change_x, change_y, radius, color)
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
     def update(self):
         """ Code to control the ball's movement. """
 
@@ -124,7 +74,6 @@ class Floc(Ball):
         if self.position_y < self.radius:
             self.position_y = SCREEN_HEIGHT 
             
-<<<<<<< HEAD
 def ninot (px,py):
     #cos
     arcade.draw_circle_filled(100+px, 100+py, 60, arcade.color.WHITE)
@@ -144,10 +93,24 @@ def ninot (px,py):
     #capell
     arcade.draw_lrtb_rectangle_filled(70+px, 130+px, 300+py, 260+py, arcade.csscolor.BLACK)
     arcade.draw_lrtb_rectangle_filled(50+px, 150+px, 270+py, 260+py, arcade.csscolor.BLACK)
-    
-=======
-        
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
+
+def regal (px,py):
+    arcade.draw_lrtb_rectangle_filled(px, 100+px, 100+py,py, arcade.csscolor.PINK)
+    arcade.draw_lrtb_rectangle_filled(47+px, 53+px, 100+py,py, arcade.csscolor.RED)
+    arcade.draw_lrtb_rectangle_filled(px, 100+px, 53+py,py+47, arcade.csscolor.RED)
+
+def arbre (px,py):
+    #tronc
+    arcade.draw_lrtb_rectangle_filled(285+px, 315+px, 160+py,0+py, arcade.csscolor.BROWN)
+    #fulles
+    arcade.draw_triangle_filled(300+px, 450+py, 190+px, 60+py, 410+px, 60+py, arcade.csscolor.GREEN)
+    arcade.draw_triangle_filled(300+px, 450+py, 200+px, 160+py, 390+px, 160+py, arcade.csscolor.GREEN)
+    arcade.draw_triangle_filled(300+px, 500+py, 210+px, 230+py, 380+px, 230+py, arcade.csscolor.GREEN)
+def nuvol (px,py,r):
+    arcade.draw_circle_filled(px, py, r, arcade.csscolor.WHITE)
+    arcade.draw_circle_filled(px-30, py-10, r, arcade.csscolor.WHITE)
+    arcade.draw_circle_filled(px-60, py, r, arcade.csscolor.WHITE)
+    arcade.draw_circle_filled(px-30, py+10, r, arcade.csscolor.WHITE)
 
 class MyGame(arcade.Window):
 
@@ -160,35 +123,10 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.DARK_BLUE)
 
         self.llista = []
-<<<<<<< HEAD
         
         px=0
         py=0
-        """
-        self.llista.append(Ninot(100+px, 100+py, 60, 100+py, 60, arcade.color.WHITE))
-        self.llista.append(Ninot(100+px, 170+py, 50, 170+py, 50, arcade.color.WHITE))
-        self.llista.append(Ninot(100+px, 240+py, 40, 240+py, 40, arcade.color.WHITE))
 
-        self.llista.append(Ninot(80+px, 250+py, 5, 250+py, 5, arcade.color.BLACK))
-        self.llista.append(Ninot(120+px, 250+py, 5, 250+py, 5, arcade.color.BLACK))
-        self.llista.append(Ninot(100+px, 230+py, 5, 230+py, 5, arcade.color.ORANGE))
-
-        self.llista.append(Ninot(100+px, 100+py, 5, 100+py, 5, arcade.color.BROWN))
-        self.llista.append(Ninot(100+px, 140+py, 5, 140+py, 5, arcade.color.BROWN))
-        self.llista.append(Ninot(100+px, 180+py, 5, 180+py, 5, arcade.color.BROWN))
-
-        self.llista.append(Ninot(60+px, 140+px, 215+py, 200+py, 0, arcade.csscolor.PINK))
-        self.llista.append(Ninot(125+px, 140+px, 215+py, 150+py, 0, arcade.csscolor.PINK))
-
-        self.llista.append(Ninot(70+px, 130+px, 300+py, 260+py, 0, arcade.csscolor.BLACK))
-        self.llista.append(Ninot(50+px, 150+px, 270+py, 260+py, 0, arcade.csscolor.BLACK))
-
-        self.llista.append(Tronc(285+px, 315+px, 160+py,0+py, 0, arcade.csscolor.BROWN))
-        """
-
-=======
-        px=0
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
         for i in range(10):
             self.llista.append(Floc(random.randrange(px-50, px+80),random.randrange(-50, 50), 0,random.randrange(-3,-1), random.randrange(3, 4), arcade.color.WHITE))
             self.llista.append(Floc(random.randrange(px-60, px+90),random.randrange(-150, 150), 0, random.randrange(-3,-1), random.randrange(3, 4), arcade.color.WHITE))
@@ -198,53 +136,30 @@ class MyGame(arcade.Window):
             self.llista.append(Floc(random.randrange(px-100, px+170),random.randrange(-350, 350), 0, random.randrange(-3,-1), random.randrange(3, 4), arcade.color.WHITE))
             px +=65
         
-<<<<<<< HEAD
-        #self.llista.append(Sol(300, 360, 30,10,10, arcade.csscolor.YELLOW))
-=======
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
-
-        # Attributes to store where our ball is
-        #self.ball = Floc(50, 50, 0, -1, 10, arcade.color.WHITE)
-        #self.ball2 = Floc(150, 50, 0, -2, 10, arcade.color.WHITE)
 
     
-<<<<<<< HEAD
-=======
-
-
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
+        nuvol(150,500,40)
+        nuvol(450,500,30)
+        nuvol(300,550,30)
+        regal(400,0)
+        arbre(30,0)
+        ninot(20,-40)
+        
         for i in self.llista:
             i.draw()
-<<<<<<< HEAD
-        ninot(20,20)
-=======
-        #self.ball.draw()
-        #self.ball2.draw()
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
+        
 
     def update(self, delta_time):
         """ Called to update our objects. Happens approximately 60 times per second."""
         for i in self.llista:
             i.update()
-<<<<<<< HEAD
 
 def main():
     window = MyGame(600, 600, "Drawing Example")
 
     arcade.run()
 
-=======
-        #self.ball2.update()
-        #self.ball.update()
-    
-def main():
-    window = MyGame(640, 480, "Drawing Example")
-
-    arcade.run()
-
-
->>>>>>> fff130306a4e93b6e1f46ea04d471d6579127c61
 main()
